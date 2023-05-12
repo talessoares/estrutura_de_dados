2bfbb2d8cc9617232685e80d990171cade93fbf7
Tales <talesschifelbeinsoares@gmail.com>
2023-05-12 15:22:00 -0300

    tempo agora mostrado em nanosegundos

--- Marco01/Atividade07/Main.java
+++ Marco01/Atividade07/Main.java
@@ -10,23 +10,32 @@ public class Main {
 
         for (int i = 0; i < numerosAleatorios.length; i++) {
                 vetor.add(numerosAleatorios[i]);
-                System.out.println(numerosAleatorios[i]);
+                //System.out.println(numerosAleatorios[i]);
         }       
 
 
-        long startTimeRec = System.currentTimeMillis();
+        long startTimeRec = System.nanoTime();
         System.out.println("Maximo recursivo: " + vetor.maxRec(numerosAleatorios, 0, numerosAleatorios.length-1));
-        long endTimeRec = System.currentTimeMillis();
+        long endTimeRec = System.nanoTime();
         long durationRec = endTimeRec - startTimeRec;
-        System.out.println("Tempo de execução recursividade: " + durationRec + "ms");
+        System.out.println("Tempo de execução recursividade: " + durationRec + " nanosegundos");
         
         
         
-        long startTimeIt = System.currentTimeMillis();
+        long startTimeIt = System.nanoTime();
         System.out.println("Maximo iterativo: " + vetor.maxIt(numerosAleatorios.length));
-        long endTimeIt = System.currentTimeMillis();
+        long endTimeIt = System.nanoTime();
         long durationIt = endTimeIt - startTimeIt;
-        System.out.println("Tempo de execução iterativo: " + durationIt + "ms");
+        System.out.println("Tempo de execução iterativo: " + durationIt + " nanosegundos");
+
+        if (durationRec > durationIt) {
+            System.out.println("Iterativo é mais rápido");
+            System.out.println("Diferença de tempo: " + (durationRec - durationIt) + " nanosegundos");
+        }
+        else {
+            System.out.println("Recursivo é mais rápido");
+                System.out.println("Diferença de tempo: " + (durationIt - durationRec) + " nanosegundos");
+        }
 
         input.close();
 
